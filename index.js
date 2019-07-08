c8985f2573839433403eb32b6346399881e8a9e7
Ionut-Cristian Florescu <ionut.florescu@gmail.com>
2019-07-08 12:53:41 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+// prettier-ignore
+module.exports = {
+  root: true,
+  env: { es6: true, node: true },
+  parserOptions: { ecmaVersion: 10, sourceType: 'script' },
+  extends: [
+    'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'airbnb-base',
+    'prettier'
+  ],
+  plugins: ['import', 'prettier'],
+  rules: {
+    'strict': 'off',
+    'indent': 'error',
+    'comma-dangle': ['error', 'never'],
+    'max-len': ['warn', 120],
+    'prefer-const': 'warn',
+    'camelcase': 'off',
+    'arrow-parens': ['error', 'as-needed', { requireForBlockBody: true }],
+    'function-paren-newline': ['error', 'consistent'],
+    'newline-per-chained-call': 'off',
+    'quotes': ['warn', 'single', { avoidEscape: true, allowTemplateLiterals: true }],
+    'quote-props': ['error', 'consistent'],
+    'space-in-parens': ['error', 'never'],
+    'object-curly-newline': ['error', { consistent: true, multiline: true }],
+    'object-curly-spacing': ['error', 'always'],
+    'semi': ['error', 'always'],
+    'key-spacing': 'error',
+    'no-param-reassign': 'off',
+    'no-multi-assign': 'off',
+    'radix': 'off',
+    'global-require': 'off',
+    'import/no-dynamic-require': 'off'
+  }
+};
